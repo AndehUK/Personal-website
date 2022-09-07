57acd1132a5bee7e790eb0b74d29a1de4d8a53ad
Andy <andrewg.mason2709@gmail.com>
2022-09-07 16:50:45 +0100

    .

--- static/js/main.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector("button.mobile-menu-button");
-const menu = document.querySelector(".mobile-menu");
-
-button.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-    menu.classList.toggle("absolute");
-    menu.classList.toggle("w-full");
-});
