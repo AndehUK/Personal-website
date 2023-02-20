beb25154c81e6d12fab4c7d2fadea5661e037d4b
ExHiraku <andy@andeh.uk>
2023-02-20 01:48:59 +0000

    About + Projects sections complete

---
+++ andeh-uk/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import About from "./components/about";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+    </>
+  );
+}
+
+export default App;
